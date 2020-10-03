a5d232468c6ae3d706a7e96763a7989f1bfb9be3
Varis Darasirikul <varis.dara@gmail.com>
2020-10-03 23:32:09 +0800

    Fix coffee service dep

--- src/coffees/coffees.service.spec.ts
+++ src/coffees/coffees.service.spec.ts
@@ -1,11 +1,37 @@
+import { ConfigModule } from '@nestjs/config';
+import { EventSchema } from './../events/entities/event.entity';
+import { Coffee, CoffeeSchema } from './entities/coffee.entity';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoffeesService } from './coffees.service';
+import { Event } from '../events/entities/event.entity';
 
 describe('CoffeesService', () => {
   let service: CoffeesService;
 
   beforeEach(async () => {
+    //
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule,
+        MongooseModule.forRoot('mongodb://localhost:27017/nest-course', {
+          useNewUrlParser: true,
+          useFindAndModify: true,
+          useCreateIndex: true,
+          useUnifiedTopology: true,
+        }),
+        /* Add Schema to MongooseModule in CoffeesModule */
+        MongooseModule.forFeature([
+          {
+            name: Coffee.name,
+            schema: CoffeeSchema,
+          },
+          {
+            name: Event.name,
+            schema: EventSchema,
+          },
+        ]),
+      ],
       providers: [CoffeesService],
     }).compile();
 
